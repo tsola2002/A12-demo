9721a1bc265b286984d96d193f44db8a130187cc
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2021-10-22 08:49:40 +0100

    using sha256 to encrypt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as crypto from 'crypto-js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'A12-Demo-Apps';
+
+  order_id: string = 'test';
+  payment_id: string = 'payment';
+  generated_hash: any = '';
+  generated_hash2: any = '';
+
+  ngOnInit() {
+    this.generated_hash = crypto.SHA256(this.order_id).toString(crypto.enc.Hex);
+    console.log('GENERATED SIGNATURE TEST', this.generated_hash);
+
+    this.generated_hash2 = crypto.SHA256(this.payment_id).toString(crypto.enc.Hex);
+    console.log('GENERATED SIGNATURE PAYMENT', this.generated_hash2);
+
+
+  }
+
+
+
 }
