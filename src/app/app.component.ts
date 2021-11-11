a2a5bdcafbcf6516b584d52080ec0594d0bd0376
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2021-11-11 09:57:00 +0100

    stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component,} from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { map, merge } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent OnInit, OnDestroy {
   title = 'A12-Demo-Apps';
+
+  private subscription: Subscription = null;
+
+  inputStreamData = ['john wick', 'inception', 'interstellar'];
+  cartoonStreamData = ['Thunder Cats', 'Dragon Ball Z', 'Ninja Turtles'];
+  outputStreamData = [];
+
+  ngOnInit() {
+  }
+
+   constructor (){}  
+
+  // We have to make sure that the mapped numbers are not greater than or equal to the length of inputStreamData.
+  // We'll do this by taking a modulus on the number each time,
+  // using the map operator as follows
+  startStream(){
+    const streamSource = interval(1500);
+    const cartoonStreamSource = interval(1000);
+
+    this.subscription = streamSource
+        .pipe(
+          map(output => output % this.inputStreamData.length),
+          // we'll use another map method to fetch an element from the array for each of the stream's outputs,
+          //as follow
+          map(index => this.inputStreamData[index]),
+          merge(cartoonStreamSource)
+        ) 
+      .subscribe(element => {
+          this.outputStreamData.push(element);
+      });
+
+  }
+
+  stopStream() {
+    this.subscription.unsubscribe();
+    this.subscription = null;
+  }
+
 }
